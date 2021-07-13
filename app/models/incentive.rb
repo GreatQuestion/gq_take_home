0dcf62c2fc3df8be2de2314799c55c47c19d9a62
James Dullaghan <support@realvestor.io>
2021-07-12 18:08:45 -0600

    Add validations to incentive

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,4 +1,5 @@
 # Incentive is the coupon set by the researcher (person who is creating the campaign or whatever resoruce)
 class Incentive < ApplicationRecord
   has_many :candidate_incentives, dependent: :destroy
+  validates :code, presence: true, uniqueness: true
 end
