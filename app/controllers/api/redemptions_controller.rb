dcc5963146c85b7b0bbdc90661812b83cf482945
James Dullaghan <support@realvestor.io>
2021-07-12 18:11:36 -0600

    Add basic spike for redemptions controller without testing logic

--- app/controllers/api/redemptions_controller.rb
+++ app/controllers/api/redemptions_controller.rb
@@ -1,11 +1,29 @@
 class Api::RedemptionsController < ::Api::BaseController
+  # GET /api/incentives/:incentive_id/redemptions
   def index
     render json: { redeemed: redeemed }, status: :ok
   end
 
   private
 
+  # Incentive for the supplied ID
+  #
+  # @return [<Incentive>]
+  def incentive
+    Incentive.find_by!(id: params[:incentive_id])
+  end
+
+  # Candidate inventives for the incentive
+  #
+  # @return [ActiveRecord::Collection<CandidateIncentive>]
+  def candidate_incentives
+    incentive.candidate_incentives
+  end
+
+  # The redeemed candidate incentives based on the incentive
+  #
+  # @return [ActiveRecord::Collection<CandidateIncentive>]
   def redeemed
-    Incentive.redeemed
+    candidate_incentives.redeemed_incentives
   end
 end
