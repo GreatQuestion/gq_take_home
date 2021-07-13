b3a8e6b2cd31d3c0ebf1fc6e2cb6ff095be72867
James Dullaghan <support@realvestor.io>
2021-07-12 18:09:49 -0600

    Add some logic for the incentives controller and add some endpoints

--- app/controllers/api/incentives_controller.rb
+++ app/controllers/api/incentives_controller.rb
@@ -1,21 +1,51 @@
 class Api::IncentivesController < ApplicationController
-
   def index
-    @incentives = Incentive.all
-    
-    render json: @incentives.to_json
+    render json: {
+      incentives: incentives,
+      allCodes: ::Secret::CodeService.all_codes,
+    }, status: :ok
   end
 
-  def update
-    @incentive = Incentive.find(params[:id])
+  def create
+    if update_params[:code].present?
+      data = service.perform
 
-    @incentive.update!(update_params)
-    render json: @incentive.to_json
+      ::Incentive.upsert_all(data, unique_by: :code)
+
+      render json: {
+        incentives: incentives,
+        allCodes: ::Secret::CodeService.all_codes,
+      }, status: :created
+    else
+      render json: {
+        errors: 'Unable to create incentives',
+      }, status: :unprocessable_entity
+    end
+  end
+
+  # TODO : See if update works
+  def update
+    data = service.perform
+    incentive.update(data)
+
+    render json: incentive
   end
 
   private
 
   def update_params
     params.require(:incentive).permit(:code)
+  end
+
+  def service
+    @_service ||= ::Secret::CodeService.new(codes: update_params[:code])
+  end
+
+  def incentives
+    @_incentives ||= Incentive.all
+  end
+
+  def incentive
+    @_incentive ||= Incentive.find(params[:id])
   end
 end
