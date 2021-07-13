6183061955c5880e7b16d15eace9bdd436c846da
James Dullaghan <support@realvestor.io>
2021-07-12 18:05:51 -0600

    Move seed logic to service

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-::CODES = %w(8xWnvk6a)
-
-::CODES.each do |code|
-  incentive = Incentive.create(code: code)
-
-  4.times do |i|
-    incentive.candidate_incentives.create!
-  end
-end
+CodeGenerationService.perform
