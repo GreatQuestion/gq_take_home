b48a657dbafbee5884ca858cd87329b61dd9ea14
James Dullaghan <support@realvestor.io>
2021-07-12 20:55:11 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :incentives, only: [:index, :create] do
-      resources :redemptions, only: [:index]
+      resources :redemptions, only: [:create]
     end
   end
 
-  get :redeem, to: 'redemptions#index'
-  get :setup, to: 'setups#index'
+  get '/redeem', to: 'redemptions#index', as: :redeem
+  get '/redeem/:code', to: 'redemptions#show', as: :redemption
+  get '/setup', to: 'setups#index', as: :setup
 
   root to: 'home#index'
 end
