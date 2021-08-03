f79e7d97111061c530604625072ee54569c0b134
Rhys Gevaux <rhys-github@sandimas.co>
2021-08-03 19:57:16 +1000

    Use useIncentives in CandidateApp

--- app/javascript/components/CandidateApp/CandidateApp.tsx
+++ app/javascript/components/CandidateApp/CandidateApp.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import { getIncentives } from "@api/endpoints";
 import { Redeem } from "./Redeem";
+import { useIncentives } from "../../hooks";
 
 export const CandidateApp: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<Incentive[]>();
-
-  useEffect(() => {
-    getIncentives()
-      .then(setData)
-      .finally(() => setLoading(false));
-  }, []);
+  const state = useIncentives();
 
   return (
     <div className="px-12 py-6">
       <h1 className="text-2xl font-bold mb-6">Redeem incentive</h1>
 
-      {loading && <span>Loading...</span>}
-
-      {!loading && data && <Redeem data={data} />}
+      {state.status === "loading" ? <span>{"Loading..."}</span> : null}
+      {state.status === "error" ? <span>{"An error occurred"}</span> : null}
+      {state.status === "loaded" ? <Redeem data={state.incentives} /> : null}
     </div>
   );
 };
