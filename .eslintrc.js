f84b36297c8bb10d5f5329ad956fe927887a07df
Rhys Gevaux <rhys-github@sandimas.co>
2021-08-03 11:49:02 +1000

    Fix eslint cfg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'
-  ],
+  env: {
+    commonjs: true,
+    es6: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  reportUnusedDisableDirectives: true,
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      env: { node: true },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+    {
+      files: ["app/javascript/**"],
+      env: { browser: true },
+    },
+    {
+      files: ["app/javascript/**/*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
 };
